26b0c6c6cc2eb5f4556e6c4db09add45e123bdd5
jeongminchan <104063175+MinchoLover@users.noreply.github.com>
2024-11-07 18:20:21 +0900

    [feat] initial commit

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -76,6 +76,7 @@ export async function POST(request: Request) {
 
   if (!chat) {
     const title = await generateTitleFromUserMessage({ message: userMessage });
+
     await saveChat({ id, userId: session.user.id, title });
   }
 
